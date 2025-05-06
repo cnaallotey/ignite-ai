d6a0fc88d51cfae111798a9938caddaf3e6d8fd9
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2025-05-06 23:09:42 +0000

    update

--- pages/course/register.vue
+++ pages/course/register.vue
@@ -68,10 +68,12 @@ const submit = async () => {
 <template>
   <div class="w-full flex-1 flex flex-col">
     <div class="max-w-4xl mx-auto border-x border-slate-800 px-10">
-      <h1 class="text-white pt-10 lg:pt-40 font-mendium text-3xl sm:text-5xl pb-5"
-      :class="{'!text-center': success}">
-        {{ success ? "Congratulations on joining the" : "Register for" }}
-        {{ course?.name }}
+      <h1
+        class="text-white pt-10 lg:pt-40 font-mendium text-3xl sm:text-5xl pb-5"
+        :class="{ '!text-center': success }"
+      >
+        {{ success ? "Congratulations on joining the" : "Register for the" }}
+        {{ course?.name }} Class
       </h1>
     </div>
     <div class="border-y border-slate-800 flex-1">
@@ -82,22 +84,22 @@ const submit = async () => {
             <br /><br />
             To download your admission letter directly, please click the button below:
           </p>
-         <div class="grid gap-5 max-w-2xl mt-12 mx-auto">
+          <div class="grid gap-5 max-w-2xl mt-12 mx-auto">
             <NuxtLink
-            :to="course['letter-download-link']"
-            target="_blank"
-            class="bg-pink-600 relative text-lg text-center px-8 py-3 md:py-3 font-medium text-white"
-          >
-            Download Admission Letter
-          </NuxtLink>
-          <NuxtLink
-          :to="course['brochure-download-link']"
-            target="_blank"
-            class="bg-pink-600 relative text-center text-lg px-8 py-3 md:py-3 font-medium text-white"
-          >
-            Download Course Brochure
-          </NuxtLink>
-         </div>
+              :to="course['letter-download-link']"
+              target="_blank"
+              class="bg-pink-600 relative text-lg text-center px-8 py-3 md:py-3 font-medium text-white"
+            >
+              Download Admission Letter
+            </NuxtLink>
+            <NuxtLink
+              :to="course['brochure-download-link']"
+              target="_blank"
+              class="bg-pink-600 relative text-center text-lg px-8 py-3 md:py-3 font-medium text-white"
+            >
+              Download Course Brochure
+            </NuxtLink>
+          </div>
         </div>
         <form v-else @submit.prevent="submit" class="grid gap-5">
           <div class="">
@@ -130,40 +132,61 @@ const submit = async () => {
             <label for="phone-number" class="block text-sm font-medium mb-2 text-white"
               >Phone Number</label
             >
-            <input
-              type="tel"
-              id="phone-number"
-              v-model="userData.contact"
-              required
-              class="py-2.5 sm:py-4 px-4 text-white block w-full border border-slate-800 sm:text-sm disabled:opacity-50 focus:ouline-none disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              placeholder="+233 xxx xxx xxx"
-            />
+            <ClientOnly>
+              <vue-tel-input
+                v-model="userData.contact"
+                mode="international"
+                v-bind="{
+                  inputOptions: {
+                    placeholder: 'Enter your phone number',
+                    required: true,
+                    id: 'phone-number',
+                    required: true,
+                    styleClasses: '!text-white h-14 ',
+                  },
+                }"
+                class="text-sm w-full rounded-lg !border-slate-800"
+              />
+            </ClientOnly>
           </div>
           <div class="">
             <label for="whatsapp-number" class="block text-sm font-medium mb-2 text-white"
               >Whatsapp Number</label
             >
-            <input
-              type="tel"
-              id="whatsapp-number"
-              v-model="userData.whatsapp"
-              required
-              class="py-2.5 sm:py-4 px-4 text-white block w-full border border-slate-800 sm:text-sm disabled:opacity-50 focus:ouline-none disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              placeholder="+233 xxx xxx xxx"
-            />
+            <ClientOnly>
+              <vue-tel-input
+                v-model="userData.whatsapp"
+                mode="international"
+                v-bind="{
+                  inputOptions: {
+                    placeholder: 'Enter your Whatsapp number',
+                    required: true,
+                    id: 'phone-number',
+                    required: true,
+                    styleClasses: '!text-white h-14',
+                  },
+                }"
+                class="text-sm w-full rounded-lg !border-slate-800"
+              />
+            </ClientOnly>
+            <pre class="text-white">{{ userData.whatsapp }}</pre>
+            <p v-show="userData.whatsapp?.length" class="text-white/80 mt-2 text-sm">
+              Please ensure Whatsapp number is active. The Thrive Team will contact you to
+              provide further information via Whatsapp.
+            </p>
           </div>
-          <div id="country" class="relative">
-            <label for="country" class="block text-sm font-medium mb-2 text-white"
+          <div id="country-container" class="relative">
+            <label for="country-input" class="block text-sm font-medium mb-2 text-white"
               >Country</label
             >
             <input
-              list="country"
-              id="country"
+              list="country-list"
+              id="country-input"
               v-model="userData.country"
               required
-              class="py-2.5 sm:py-4 px-4 text-white block w-full border border-slate-800 sm:text-sm disabled:opacity-50 focus:ouline-none disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              class="py-2.5 sm:py-4 px-4 text-white block w-full border border-slate-800 sm:text-sm disabled:opacity-50 focus:outline-none disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             />
-            <datalist id="country">
+            <datalist id="country-list">
               <option
                 v-for="item in payload.countriesData.data"
                 :value="item.country"
